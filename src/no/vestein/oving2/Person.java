00541027c0dce06900fa3ba45bba5e4c3f5e698d
WoodStone <golfflames@gmail.com>
2015-02-02 21:32:54 +0100

    Øving 2 Cleanup

--- src/no/vestein/oving2/Person.java
+++ src/no/vestein/oving2/Person.java
@@ -2,46 +2,28 @@ package no.vestein.oving2;
 
 import java.util.Date;
 import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Person {
 
-//	private String fornavn;
-//	private String etternavn;
+	private String fornavn;
+	private String etternavn;
 	private String name;
 	private String email;
 	private Date birthday;
 	private char gender;
-	
-//	private final String email_regex = "\\w+\\.\\w+@\\w+\\.\\w{2}";
-//	private final Pattern email_pattern = Pattern.compile(email_regex);
-	
-
-	@Override
-	public String toString() {
-		return "Person [name=" + name + ", email=" + email + ", birthday="
-				+ birthday + ", gender=" + gender + "]";
-	}
 
 	public void setName(String name) {
 		if (checkName(name)) { 
-//			String[] s = name.split("\\s");
-//			fornavn = s[0];
-//			etternavn = s[1];
+			String[] s = name.split("\\s");
+			fornavn = s[0].toLowerCase();
+			etternavn = s[1].toLowerCase();
 			this.name = name;
 		}
 	}
 	
-	private boolean checkName(String s) {
-		String[] splitString =  s.split("\\s");
-		if (splitString.length != 2) {
-			throw new IllegalArgumentException("invalid name");
-		} else if (splitString[0].matches("[a-zA-Z]+") && splitString[0].length() >= 2 && splitString[1].matches("[a-zA-Z]+") && splitString[1].length() >= 2) {
-			return true;
-		} else {
-			throw new IllegalArgumentException("invalid name");
-		}
+	private boolean checkName(String name) {
+		if (name.matches("[a-zA-Z]{2,}\\s[a-zA-Z]{2,}")) return true;
+		throw new IllegalArgumentException("invalid name");
 	}
 	
 	public void setEmail(String email) {
@@ -49,23 +31,15 @@ public class Person {
 	}
 	
 	private boolean checkEmail(String email) {
-		//Matcher emailMatcher = email_pattern.matcher(email);
-		//if (emailMatcher.matches()) {
-		
 		if (email.matches("\\w+\\.\\w+@\\w+\\.\\w{2}")) {
-			if (name != null) {
-				String[] splitName =  name.split("\\s");
-				if (email.matches(splitName[0].toLowerCase() + "\\." + splitName[1].toLowerCase() + "@\\w+\\.\\w{2}")) {
-					String[] splitString = email.split("\\.");
-					String[] locales = Locale.getISOCountries();
-					for (String landskode : locales) {
-						if (splitString[2].equals(landskode.toLowerCase())) return true;
-					}
-				} else {
-					throw new IllegalStateException("invalid email, compared to name");
+			if (name == null) return true;
+			if (email.contains(fornavn + "." + etternavn)) {
+				String[] locales = Locale.getISOCountries();
+				for (String landskode : locales) {
+					if (email.endsWith(landskode.toLowerCase())) return true;
 				}
 			} else {
-				return true;
+				throw new IllegalStateException("invalid email");
 			}
 		}
 		throw new IllegalArgumentException("invalid email");
@@ -94,15 +68,5 @@ public class Person {
 	public Date getBirthday() { return birthday; }
 	
 	public char getGender() { return gender; }
-	
-	public static void main(String[] args) {
-		Person test = new Person();
-		
-		System.out.println(test);
-		test.setName("Vestein Dahl");
-		System.out.println(test);
-		test.setEmail("vestein.dahl@gmail");
-		System.out.println(test);
-	}
 	
 }
